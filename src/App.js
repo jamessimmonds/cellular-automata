0e518be8e11f9a8b7c431cb5de70b3a8ef70231b
James Simmonds <james.e.simmonds@gmail.com>
2021-03-27 21:36:14 +0000

    Minor change to wording on page

--- src/App.js
+++ src/App.js
@@ -38,8 +38,7 @@ function App() {
   return (
     <div>
       <h1>Game of life</h1>
-      <p>See page on <a href="https://en.wikipedia.org/wiki/Cellular_automaton">Wikipedia</a></p>
-      <p>Source on <a href="https://github.com/jamessimmonds/cellular-automata">Github</a></p>
+      <p>Source and README on <a href="https://github.com/jamessimmonds/cellular-automata">Github</a></p>
       <form>
         <fieldset>
           <legend>Controls</legend>
