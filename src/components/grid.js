ab22fbf655040afedd30ddb2cd78f696631c2123
James Simmonds <james.e.simmonds@gmail.com>
2021-03-27 18:08:34 +0000

    State pushed up

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,11 +1,6 @@
-import randomStart from "../utils/random-start.js";
 import GridRow from "./grid-row.js";
-import iteration from "../utils/rules.js";
 
-function Grid ( {rows, cols, proportion} ) {
-
-    const data = randomStart(rows, cols, proportion);
-    iteration(data);
+function Grid ( {rows, cols, proportion, data} ) {
     
     const gridRows = data.map((row, index) => 
         <GridRow key={index} data={row} />
